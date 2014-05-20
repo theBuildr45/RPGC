f16cd35e308a4fbadaff71d715713c93b090ebfc
MonoSkyStep <jbofl10@gmail.com>
2014-05-20 12:04:46 -0400

    updated Weapons.java .7

--- RPGClass/src/com/skystep/RPGClass/Weapons/Weapons.java
+++ RPGClass/src/com/skystep/RPGClass/Weapons/Weapons.java
@@ -2,11 +2,38 @@ package com.skystep.RPGClass.Weapons;
 
 public class Weapons
 {
+
+	/*
+	* Self reminder**
+	* this class is an
+	* instance of a weapon
+	* so basicialy a weapon
+	* itself. When taking
+	* a String WeaponName
+	* it is just
+   */
 	
 	Weapons weapon;
 
-	
-	public Weapons(Weapons w)
+	private String Name;
+
+	private String Lore;
+
+	private Double WeaponDamage;
+
+	private int WeaponUsage;
+
+	private String Description;
+
+	private String[] Abilities;
+
+
+
+	/*
+	 * No idea what todo
+	 here really but 
+	*/
+	public Weapons(String weapon)
 	{}
 	
 	
